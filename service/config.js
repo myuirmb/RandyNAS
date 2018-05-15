3e0be590c8da5a1d70667b426e491f3e944b123f
myuirmb <myuirmb@gmail.com>
2018-05-15 12:59:08 +0800

    config.js

--- service/config.js
+++ service/config.js
@@ -6,26 +6,26 @@ const lcd = require('./config/log.config.dev');
 //set system path is dev;
 const _sys_ = 'dev';
 
-//ct:(undefind) default value is config
-//any value is log
-const config = (ct) => {
-    if (_sys_) {
-        if (ct) {
-            return lcd;
-        }
-        else {
-            return cd;
-        }
-    }
-    else {
-        if (ct) {
-            return lcp;
-        }
-        else {
-            return cp;
-        }
+// const config = (ct) => {
+//     if (_sys_) {
+//         if (ct) {
+//             return lcd;
+//         }
+//         else {
+//             return cd;
+//         }
+//     }
+//     else {
+//         if (ct) {
+//             return lcp;
+//         }
+//         else {
+//             return cp;
+//         }
 
-    }
-};
+//     }
+// };
+
+const config = (ct) => _sys_ ? (ct ? lcd : cd) : (ct ? lcp : cp);
 
 module.exports = config;
